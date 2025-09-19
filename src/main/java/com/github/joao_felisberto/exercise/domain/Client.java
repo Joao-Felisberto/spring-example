450152c7c913b45559251db69193a5c02446368d
Joao-Felisberto <joaofelisberto@tutamail.com>
2025-09-19 08:51:43 +0100

    [spring-example#1] added Client struct

---
+++ src/main/java/com/github/joao_felisberto/exercise/domain/Client.java
@@ -0,0 +1,134 @@
+package com.github.joao_felisberto.exercise.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * A Client.
+ */
+@Entity
+@Table(name = "client")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nif")
+    private Integer nif;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private Integer phone;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Client id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Client name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNif() {
+        return this.nif;
+    }
+
+    public Client nif(Integer nif) {
+        this.setNif(nif);
+        return this;
+    }
+
+    public void setNif(Integer nif) {
+        this.nif = nif;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Client address(String address) {
+        this.setAddress(address);
+        return this;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getPhone() {
+        return this.phone;
+    }
+
+    public Client phone(Integer phone) {
+        this.setPhone(phone);
+        return this;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Client)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Client) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Client{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", nif=" + getNif() +
+            ", address='" + getAddress() + "'" +
+            ", phone=" + getPhone() +
+            "}";
+    }
+}
