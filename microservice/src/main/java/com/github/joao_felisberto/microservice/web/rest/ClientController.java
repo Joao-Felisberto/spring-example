2c097b8926d04a2318d951986e493860add7a3c8
Joao-Felisberto <joaofelisberto@tutamail.com>
2025-10-02 09:23:12 +0100

    [#2] Added DELETE endpoint

--- microservice/src/main/java/com/github/joao_felisberto/microservice/web/rest/ClientController.java
+++ microservice/src/main/java/com/github/joao_felisberto/microservice/web/rest/ClientController.java
@@ -15,10 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.jhipster.web.util.HeaderUtil;
 
 import java.net.URI;
@@ -37,7 +34,7 @@ import java.net.URISyntaxException;
 @Transactional
 public class ClientController implements ClientApiDelegate {
     private static final Logger LOG = LoggerFactory.getLogger(ClientController.class);
-    private static final String ENTITY_NAME =  Client.class.getName();
+    private static final String ENTITY_NAME = Client.class.getName();
 
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
@@ -56,15 +53,13 @@ public class ClientController implements ClientApiDelegate {
      *
      * @param clientDTO the client to create.
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new client,
-     *          or with status {@code 400 (Bad Request)} if the client has already an ID.
+     * or with status {@code 400 (Bad Request)} if the client has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect because the generated ID is not well formed.
      */
     @PostMapping("")
     @Override
     public ResponseEntity<ClientDTO> postClient(@Valid @RequestBody ClientDTO clientDTO) /*throws URISyntaxException*/ {
-        LOG.info("Got DTO : {}", clientDTO);
         final Client client = Client.fromDTO(clientDTO);
-        LOG.info("POST request to save Client : {}", client);
 
         if (client.getId() != null) {
             LOG.error("A new client cannot already have an ID");
@@ -81,7 +76,22 @@ public class ClientController implements ClientApiDelegate {
                 .body(clientRes.toDTO());
         } catch (URISyntaxException e) {
             LOG.error("Malformed URI: '{}'", "/api/clients/" + client.getId());
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * {@code DELETE  /clients} : Delete an existing client.
+     *
+     * @param id the id of the Client to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (No Content)},
+     * or {@code 404 (Not Found)} if no id is provided
+     */
+    @DeleteMapping("/{id}")
+    @Override
+    public ResponseEntity<Void> deleteClient(@PathVariable("id") Long id) {
+        LOG.debug("REST request to delete Client : {}", id);
+        clientRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
